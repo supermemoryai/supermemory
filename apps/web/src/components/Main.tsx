4400648205ea3f6172ed818b5e952556368ca913
Yash <yashverma30115@gmail.com>
2024-04-02 10:51:44 +0000

    filter combo box

--- apps/web/src/components/Main.tsx
+++ apps/web/src/components/Main.tsx
@@ -1,8 +1,12 @@
-import { SpaceIcon } from "@/assets/Memories";
+"use client";
+import { useState } from "react";
+import { FilterCombobox } from "./Sidebar/FilterCombobox";
 import { Textarea2 } from "./ui/textarea";
-import { ArrowRight, ChevronDown, GlobeIcon } from "lucide-react";
+import { ArrowRight } from "lucide-react";
 
 export default function Main() {
+  const [value, setValue] = useState("");
+
   return (
     <main className="flex h-screen w-full items-center justify-center px-60">
       <Textarea2
@@ -10,20 +14,16 @@ export default function Main() {
         textAreaProps={{
           placeholder: "Ask your SuperMemory...",
           className: "text-lg p-2 text-rgray-11",
+          value,
+          onChange: (e) => setValue(e.target.value),
         }}
       >
-        <div className="text-rgray-11/70 flex w-full items-center justify-center p-2">
-          <button className="text-rgray-11/70 focus-visible:ring-rgray-8 hover:bg-rgray-3 flex items-center justify-center gap-1 rounded-md px-2 py-1 ring-2 ring-transparent focus-visible:outline-none">
-            <SpaceIcon className="mr-1 h-5 w-5" />
-            Spaces
-            <ChevronDown className="h-4 w-4" />
-          </button>
-          <button className="text-rgray-11/70 focus-visible:ring-rgray-8 hover:bg-rgray-3 flex items-center justify-center gap-1 rounded-md px-2 py-1 ring-2 ring-transparent focus-visible:outline-none">
-            <GlobeIcon className="mr-1 h-4 w-4" />
-            Pages
-            <ChevronDown className="h-4 w-4" />
-          </button>
-          <button className="text-rgray-11/70 bg-rgray-3 focus-visible:ring-rgray-8 hover:bg-rgray-4 ml-auto flex items-center justify-center rounded-full p-2 ring-2 ring-transparent focus-visible:outline-none">
+        <div className="text-rgray-11/70 flex w-full items-center justify-center p-2 pl-0">
+          <FilterCombobox />
+          <button
+            disabled={value.trim().length < 1}
+            className="text-rgray-11/70 bg-rgray-3 focus-visible:ring-rgray-8 hover:bg-rgray-4 ml-auto flex items-center justify-center rounded-full p-2 ring-2 ring-transparent focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
+          >
             <ArrowRight className="h-5 w-5" />
           </button>
         </div>
