a23fd73a0babe7c6121be00ec261a9ab5aceecd8
Yash <yashverma30115@gmail.com>
2024-04-04 08:34:39 +0000

    fix content paths

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -2,11 +2,7 @@ import type { Config } from "tailwindcss";
 import plugin from "tailwindcss/plugin";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       keyframes: {
