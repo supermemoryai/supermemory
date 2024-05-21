c0d65851d4ba0f1d5e2bb1abad9c2aca81687843
codetorso <pruthvirajthinks@gmail.com>
2024-05-21 08:54:39 +0530

    navbar hides on scroll + twitter tag decorated

--- apps/web-v2/src/app/(landing)/Hero.tsx
+++ apps/web-v2/src/app/(landing)/Hero.tsx
@@ -4,6 +4,7 @@ import { motion } from "framer-motion";
 import { Twitter } from "@/utils/icons";
 import EmailInput from "./EmailInput";
 import LinkArrow from "./linkArrow";
+import { TwitterBorder } from "./twitterBorder";
 
 const slap = {
   initial: {
@@ -22,13 +23,7 @@ function Hero() {
   return (
     <>
       <section className="mt-24 flex max-w-xl flex-col items-center justify-center gap-10 md:mt-56">
-        <a
-          className="group/anchor pl-10 pr-6 flex items-center justify-center gap-4 rounded-full text-white/80 bg-white/10 py-2 text-sm"
-          href="https://twitter.com/supermemoryai"
-          target="_blank"
-        >
-          <Twitter className="h-4 w-4" /><div className="flex"> Follow us on Twitter <LinkArrow classname="group-hover/anchor:opacity-100 opacity-0 transition" stroke="#ffffff" /></div>
-        </a>
+        <TwitterBorder />
         <motion.h1
           {...{
             ...slap,
--- apps/web-v2/src/app/(landing)/Navbar.tsx
+++ apps/web-v2/src/app/(landing)/Navbar.tsx
@@ -1,3 +1,12 @@
+"use client";
+
+import { useState } from "react";
+import {
+  motion,
+  AnimatePresence,
+  useScroll,
+  useMotionValueEvent,
+} from "framer-motion";
 import Logo from "@/../public/logo.svg";
 import { Github } from "@/utils/icons";
 import Image from "next/image";
@@ -6,7 +15,6 @@ import React from "react";
 
 function Navbar() {
   return (
-    <nav className="fixed top-0 z-[99999] mt-12 hidden w-full px-24 text-sm md:flex">
       <div className="flex w-full flex-row justify-between rounded-2xl bg-white/10 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] backdrop-blur-lg backdrop-filter">
         <Link href={"/"} className="flex items-center p-3 opacity-50">
           <Image src={Logo} alt="Supermemory logo" width={40} height={40} />
@@ -30,8 +38,46 @@ function Navbar() {
           Open source
         </Link>
       </div>
-    </nav>
   );
 }
 
-export default Navbar;
+
+
+export const FloatingNav = () => {
+  const { scrollYProgress } = useScroll();
+
+  const [visible, setVisible] = useState(true);
+
+  useMotionValueEvent(scrollYProgress, "change", (current) => {
+    // Check if current is not undefined and is a number
+    if (typeof current === "number") {
+      let direction = current! - scrollYProgress.getPrevious()!;
+        if (direction < 0) {
+          setVisible(true);
+        } else {
+          setVisible(false);
+        }
+    }
+  });
+
+  return (
+    <AnimatePresence mode="wait">
+      <motion.nav
+        initial={{
+          opacity: 1,
+        }}
+        animate={{
+          opacity: visible ? 1 : 0,
+        }}
+        transition={{
+          duration: 0.2,
+        }}
+        className="fixed top-0 z-[99999] mt-12 hidden w-full px-24 text-sm md:flex"
+      >
+        <Navbar />
+      </motion.nav>
+    </AnimatePresence>
+  );
+};
+
+export default FloatingNav;
