8ecbde0b22326ddc5e5d386c242c129062d162e8
codetorso <pruthvirajthinks@gmail.com>
2024-06-03 09:14:28 +0530

    Implemented Highlight in Extension

--- apps/an-extension/background.ts
+++ apps/an-extension/background.ts
@@ -1,1 +1,26 @@
-console.log('Hello from the background script!')
+chrome.runtime.onInstalled.addListener(function () {
+    let context = 'selection';
+    let title = "Supermemory - Save Highlight";
+    chrome.contextMenus.create({
+      title: title,
+      contexts: ['selection'],
+      id: context,
+    });
+});
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  if (info.menuItemId === 'selection') {
+      console.log(info.pageUrl)
+      chrome.tabs.sendMessage(tab?.id || 1, info.selectionText);
+      console.log(info.selectionText)
+
+      // you can add a link to a cf worker or whatever u want
+      // fetch("", {
+      //   method: "POST",
+      //   headers: { "Content-Type": "application/json" },
+      //   body: JSON.stringify({
+      //     data: info.selectionText,
+      //   }),
+      // });
+  }
+});
--- apps/an-extension/content/content.tsx
+++ apps/an-extension/content/content.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ContentApp from './ContentApp'
 import('./base.css')
@@ -7,13 +6,10 @@ import('./content.css')
 setTimeout(initial, 1000)
 
 function initial() {
-  // Create a new div element and append it to the document's body
   const rootDiv = document.createElement('div')
   rootDiv.id = 'extension-root'
   document.body.appendChild(rootDiv)
 
-  // Use `createRoot` to create a root, then render the <App /> component
-  // Note that `createRoot` takes the container DOM node, not the React element
   const root = ReactDOM.createRoot(rootDiv)
   root.render(<ContentApp />)
 }
