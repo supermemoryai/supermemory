d376dd1519166ea9838d4bd9e168dc9873c675f3
CodeTorso <129188028+CodeTorso@users.noreply.github.com>
2024-06-12 18:26:17 -0600

    Revert "Add code for extension"

--- apps/an-extension/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['**/*.html', '**/*.tsx'],
-  theme: {
-    extend: {}
-  },
-  plugins: []
-}
-module.exports = require("@repo/tailwind-config/tailwind.config");
