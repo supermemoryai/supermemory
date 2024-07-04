7f4c1855541cc074c2d03fd6803bbb8cc9c3dab3
Kinfe123 <kinfishtech@gmail.com>
2024-07-04 10:42:13 +0300

    fix: centering the btn

--- apps/web/app/(landing)/Cta.tsx
+++ apps/web/app/(landing)/Cta.tsx
@@ -7,7 +7,7 @@ function Cta() {
   return (
     <section
       id="waitlist"
-      className="relative  bg-page-gradient dark:[box-shadow:0_-20px_80px_-20px_#8686f01f_inset] min-h-[600px] border-[1px] border-white/20 flex flex-col gap-8 justify-center items-center mt-32 mb-28 w-full md:w-3/4 rounded-3xl py-10 px-3 md:px-8 mx-auto"
+      className="relative bg-page-gradient dark:[box-shadow:0_-20px_80px_-20px_#8686f01f_inset] min-h-[600px] border-[1px] border-white/20 flex flex-col gap-8 justify-center items-center mt-32 mb-28 w-full md:w-3/4 rounded-3xl py-10 px-3 md:px-8 mx-auto"
     >
       <div className="absolute -z-1 inset-0 rounded-3xl opacity-5   h-[600px] w-full bg-transparent  bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
       <div className="absolute top-0 z-0 w-screen  right-0 mx-auto h-[500px] overflow-hidden bg-inherit  bg-[radial-gradient(ellipse_20%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
@@ -33,10 +33,10 @@ function Cta() {
       <p className="z-20 text-center text-md md:text-lg">
         Sign up for the waitlist and be the first to try Supermemory
       </p>
-      <div className="w-fit lg:w-3/4 mx-auto">
+      <div className="w-fit  mx-auto">
         <Link
           href="/signin"
-          className="inline-flex gap-x-2 justify-start items-start py-3 px-5 ml-3 w-full rounded-3xl border duration-200 sm:w-auto group bg-page-gradient border-white/30 text-md font-geistSans hover:border-zinc-600 hover:bg-transparent/10 hover:text-zinc-100 text-white z-[1] relative"
+          className="flex mx-auto w-fit gap-x-2 justify-center items-center py-3 px-5 ml-3  rounded-3xl border duration-200 group bg-page-gradient border-white/30 text-md font-geistSans hover:border-zinc-600 hover:bg-transparent/10 hover:text-zinc-100 text-white z-[1] relative"
         >
           Sign in
           <div className="flex overflow-hidden relative justify-center items-center ml-1 w-5 h-5">
