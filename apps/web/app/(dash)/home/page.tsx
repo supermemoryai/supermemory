0e323643999894a4712f1bc722fea499d14f42b6
Dhravya Shah <dhravyashah@gmail.com>
2024-07-23 23:44:55 -0500

    Update page.tsx

--- apps/web/app/(dash)/home/page.tsx
+++ apps/web/app/(dash)/home/page.tsx
@@ -121,7 +121,6 @@ function Page({ searchParams }: { searchParams: Record<string, string> }) {
 				/>
 
             <History setQuery={setQuery}/>
-            ```
 			</div>
 
 			<div className="w-full fixed bottom-0 left-0 p-4">
