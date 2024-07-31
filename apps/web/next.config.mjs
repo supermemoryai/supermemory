bf6e2e76662019967421567c7c0578163fde5d00
Dhravya Shah <dhravyashah@gmail.com>
2024-07-30 21:23:36 -0700

    nevermind. disabled sentry. again.

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -9,20 +9,6 @@ const baseNextConfig = {
 	env: {
 		TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
 	},
-	webpack: (config, { webpack }) => {
-		config.plugins.push(
-			new webpack.DefinePlugin({
-				__SENTRY_DEBUG__: false,
-				__SENTRY_TRACING__: false,
-				__RRWEB_EXCLUDE_IFRAME__: true,
-				__RRWEB_EXCLUDE_SHADOW_DOM__: true,
-				__SENTRY_EXCLUDE_REPLAY_WORKER__: true,
-			}),
-		);
-
-		// return the modified config
-		return config;
-	},
 };
 
 let selectedCofig = baseNextConfig;
