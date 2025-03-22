c027c1729c0876e0adc07bfb4bd1990cebf29632
Shreyans Jain <shreyans@shreyans.sh>
2025-03-22 07:38:23 +0530

    fix extension development id
    Signed-off-by: Shreyans Jain <shreyans@shreyans.sh>

--- apps/web/app/config/util.ts
+++ apps/web/app/config/util.ts
@@ -1,11 +1,5 @@
-// import {} from "@remix-run/cloudflare"
-
-// TODO: determine current context and return the correct extension id
+// Preserved function for now
+// TODO: move to constant
 export const getChromeExtensionId = () => {
-    // const context = 
-	// if (environment === "development") {
-	// 	return "hepmifhfbiehpdlbeigpapkmjcaphfmn";
-	// }
-    // return "lcmcmhklcjciegoakjejefbacbgikncb";
 	return "afpgkkipfdpeaflnpoaffkcankadgjfc";
 };
