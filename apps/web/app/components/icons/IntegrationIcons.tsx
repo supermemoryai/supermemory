61758b4286c47e1d7685cbb41e5e4d89f8cc4245
ronitrajfr <ronitrajofficial7@gmail.com>
2025-04-20 15:31:13 +0530

    fix #350: GitHub Icon not visible

--- apps/web/app/components/icons/IntegrationIcons.tsx
+++ apps/web/app/components/icons/IntegrationIcons.tsx
@@ -44,7 +44,7 @@ export const GithubIcon = (props: SVGProps<SVGSVGElement>) => (
 		viewBox="0 0 256 250"
 		width="1em"
 		height="1em"
-		fill="#fff"
+		fill="currentColor"
 		xmlns="http://www.w3.org/2000/svg"
 		preserveAspectRatio="xMidYMid"
 		{...props}
