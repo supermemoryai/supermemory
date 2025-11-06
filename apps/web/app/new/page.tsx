4145f35ec28bb7a380c6a6b70db15e82c70178ae
Mahesh Sanikommmu <mahesh.svmr@gmail.com>
2025-11-06 09:06:30 -0800

    add header for main layout

---
+++ apps/web/app/new/page.tsx
@@ -0,0 +1,9 @@
+import { Header } from "@/components/new/header"
+
+export default function NewPage() {
+	return (
+		<div>
+            <Header />
+		</div>
+	)
+}
--- packages/ui/components/button.tsx
+++ packages/ui/components/button.tsx
@@ -18,6 +18,8 @@ const buttonVariants = cva(
 					"border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
 				secondary:
 					"bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
+				headers:
+					"border-[#161F2C] border bg-gradient-to-b from-neutral-900 to-black !text-[14px]",
 				ghost:
 					"hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
 				link: "text-primary underline-offset-4 hover:underline",
