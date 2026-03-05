7a60efafef7851f639cc6e1077a44161c086f5df
Prasanna721 <praveenap0217@gmail.com>
2026-03-05 15:07:30 -0800

    chg

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -73,13 +73,12 @@ export default withSentryConfig(nextConfig, {
 	tunnelRoute: "/monitoring",
 
 	// Automatically tree-shake Sentry logger statements to reduce bundle size
-	disableLogger: true,
-
-	// Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-	// See the following for more information:
-	// https://docs.sentry.io/product/crons/
-	// https://vercel.com/docs/cron-jobs
-	automaticVercelMonitors: true,
+	webpack: {
+		treeshake: {
+			removeDebugLogging: true,
+		},
+		automaticVercelMonitors: true,
+	},
 })
 
 import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare"
